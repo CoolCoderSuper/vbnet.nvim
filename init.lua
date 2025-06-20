b8e24d22bdbbe889885f1b11a6182d8e9c75da05
Joseph Guenther <joseph@codingcool.ca>
2025-06-20 10:06:10 -0400

    Move again

--- init.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    pattern = "*.vb",
-    callback = function()
-        vim.opt_local.filetype = "vbnet"
-        vim.opt_local.syntax = "vbnet"
-        vim.opt_local.commentstring = "'%s"
-    end,
-})
---
+++ lua/vbnet/init.lua
@@ -0,0 +1,12 @@
+return {
+    setup = function()
+        vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+            pattern = "*.vb",
+            callback = function()
+                vim.opt_local.filetype = "vbnet"
+                vim.opt_local.syntax = "vbnet"
+                vim.opt_local.commentstring = "'%s"
+            end,
+        })
+    end
+}
