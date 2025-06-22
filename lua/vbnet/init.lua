4881b3cfdef014db33c38ee3452b6445527e7c39
sharpchen <rui.chen.sharp@gmail.com>
2025-06-22 19:09:29 +0800

    Add commentstring for vbnet

--- lua/vbnet/init.lua
+++ lua/vbnet/init.lua
@@ -3,7 +3,9 @@ local function setup()
         extension = {
             vb = function(path, _)
                 if vim.fs.root(path, function(name, _) return name:match('%.vbproj$') end) then
-                    return 'vbnet'
+                    return 'vbnet', function (buf)
+                      vim.bo[buf].commentstring = "'%s"
+                    end
                 else
                     return 'vb'
                 end
